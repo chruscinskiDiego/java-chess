ca58ad3798ee641a48b8f5e0acdf7421a2c61b9c
Diego Chruscinski <contatodiegochruscinski@gmail.com>
2023-01-22 21:11:22 -0300

    added classes: Board, Piece

--- src/application/App.java
+++ src/application/App.java
@@ -1,13 +1,13 @@
 package application;
 
+import boardGame.Board;
 import boardGame.Position;
 
 public class App {
 
     public static void main(String[] args) {
 
-        Position pos = new Position(1,2);
-        System.out.println(pos);
+        Board board = new Board(1,2);
 
 
     }
