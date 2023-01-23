7ee58807446811da596d1c25d8317c6d0d4107e1
Diego Chruscinski <contatodiegochruscinski@gmail.com>
2023-01-22 21:00:14 -0300

    Commit 1.1

--- src/application/App.java
+++ src/application/App.java
@@ -1,8 +1,14 @@
 package application;
 
+import boardGame.Position;
+
 public class App {
 
     public static void main(String[] args) {
+
+        Position pos = new Position(1,2);
+        System.out.println(pos);
+
 
     }
 }
