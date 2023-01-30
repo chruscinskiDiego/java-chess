bffe284c2369c2d8e8d347eb565133c0646d7385
Diego Chruscinski <contatodiegochruscinski@gmail.com>
2023-01-29 22:14:59 -0300

    Clear screen and ChessException/InputMissMatchException

--- src/application/App.java
+++ src/application/App.java
@@ -2,11 +2,13 @@ package application;
 
 import boardGame.Board;
 import boardGame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
 import java.sql.SQLOutput;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -19,15 +21,29 @@ public class App {
 
         while (true)
         {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+            try
+            {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
+            catch (ChessException e)
+            {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException e)
+            {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
 
         
